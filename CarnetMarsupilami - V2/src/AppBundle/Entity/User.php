9ca84373d7fe88c472abb4f77eac15d3cf72e81d
Antony Fourchault <antonyfourchault@gmail.com>
2018-02-04 11:35:07 +0100

    envoi new fonction

---
+++ CarnetMarsupilami - V2/src/AppBundle/Entity/User.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as FosUser;
+use Doctrine\Common\Collections\ArrayCollection;
+/**
+ * User
+ *
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
+class User extends FosUser
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+      * @ORM\ManyToMany(targetEntity="User")
+      */
+    private $amis;
+
+    public function __construct(){
+      parent::__construct();
+      $this->amis = new ArrayCollection();
+    }
+
+    public function addFriend(User $ami){
+      $this->amis[] = $ami;
+      return $this;
+    }
+
+    public function removeFriend(User $ami){
+      $this->amis->removeElement($ami);
+      return $this;
+    }
+
+    public function getFriends(){
+      return $this->amis;
+    }
+
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="age", type="integer")
+     */
+    private $age;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="famille", type="string", length=255)
+     */
+    private $famille;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="race", type="string", length=255)
+     */
+    private $race;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nourriture", type="string", length=255)
+     */
+    private $nourriture;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     *
+     * @return User
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return int
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set famille
+     *
+     * @param string $famille
+     *
+     * @return User
+     */
+    public function setFamille($famille)
+    {
+        $this->famille = $famille;
+
+        return $this;
+    }
+
+    /**
+     * Get famille
+     *
+     * @return string
+     */
+    public function getFamille()
+    {
+        return $this->famille;
+    }
+
+    /**
+     * Set race
+     *
+     * @param string $race
+     *
+     * @return User
+     */
+    public function setRace($race)
+    {
+        $this->race = $race;
+
+        return $this;
+    }
+
+    /**
+     * Get race
+     *
+     * @return string
+     */
+    public function getRace()
+    {
+        return $this->race;
+    }
+
+    /**
+     * Set nourriture
+     *
+     * @param string $nourriture
+     *
+     * @return User
+     */
+    public function setNourriture($nourriture)
+    {
+        $this->nourriture = $nourriture;
+
+        return $this;
+    }
+
+    /**
+     * Get nourriture
+     *
+     * @return string
+     */
+    public function getNourriture()
+    {
+        return $this->nourriture;
+    }
+
+    /**
+     * Add ami
+     *
+     * @param \AppBundle\Entity\User $ami
+     *
+     * @return User
+     */
+    public function addAmi(\AppBundle\Entity\User $ami)
+    {
+        $this->amis[] = $ami;
+
+        return $this;
+    }
+
+    /**
+     * Remove ami
+     *
+     * @param \AppBundle\Entity\User $ami
+     */
+    public function removeAmi(\AppBundle\Entity\User $ami)
+    {
+        $this->amis->removeElement($ami);
+    }
+
+    /**
+     * Get amis
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getAmis()
+    {
+        return $this->amis;
+    }
+}
